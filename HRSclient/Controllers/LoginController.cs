d0d19879a10c2b11256389c42802b3c4fddfb7d9
priyanka <preethika@LAPTOP-O5GM0QNA>
2021-01-14 23:19:30 +0530

    Add project files.

---
+++ HRSclient/Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using HRSclient.HRSref;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HRSclient.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Default
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+
+            LoginDetails L = new LoginDetails();
+            return View(L);
+        }
+        [HttpPost]
+        public ActionResult Register(LoginDetails L)
+        {
+            HRSref.Service1Client s = new Service1Client();
+            ViewBag.msg = s.InsertLogin(L);
+            return View();
+        }
+        //public ActionResult Booking()
+        //{
+        //    BookingDetails B = new BookingDetails();
+        //    return View(B);
+        //}
+        //[HttpPost]
+        //public ActionResult Booking(BookingDetails B)
+        //{
+        //    return View();
+        //}
+        //public ActionResult Payment()
+        //{
+        //    PaymentDetails P = new PaymentDetails();
+        //    return View(P);
+        //}
+        //[HttpPost]
+        //public ActionResult Payment(PaymentDetails P)
+        //{
+        //    return View();
+        //}
+    }
+}
---
+++ HRSclient/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HRSclient.MvcApplication" Language="C#" %>
